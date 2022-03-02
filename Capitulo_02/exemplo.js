cf8fe5284cc5dd9c825e1de5283158dd0625cdd7
EdiJunior88 <er.moura88@yahoo.com.br>
2022-03-02 20:06:02 -0300

    Criar um elemento JSX complexo

---
+++ Capitulo_02/exemplo.js
@@ -0,0 +1,9 @@
+const JSX = <div>
+  <h1>Teste de Título</h1>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. At, suscipit ducimus. Nemo repudiandae expedita suscipit, exercitationem cumque earum hic ex facere modi dolorem, possimus, laudantium ducimus unde illo quaerat consequatur?</p>
+  <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ul>
+</div>
