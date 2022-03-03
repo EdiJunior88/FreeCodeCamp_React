4e0b1f2463c6b0551ef709dd5399db3043c728fc
EdiJunior88 <er.moura88@yahoo.com.br>
2022-03-02 21:10:33 -0300

    Renderizar elementos HTML para o DOM

---
+++ Capitulo_04/exemplo.js
@@ -0,0 +1,8 @@
+const JSX = (
+  <div>
+    <h1>Hello World</h1>
+    <p>Lets render this to the DOM</p>
+  </div>
+);
+// Altere o código abaixo desta linha
+ReactDOM.render(JSX, document.getElementById("challenge-node"))
