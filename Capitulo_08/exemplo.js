d9a0e618b5989a1da4d898fb16a66188373ff41a
EdiJunior88 <er.moura88@yahoo.com.br>
2022-03-04 19:52:47 -0300

    Criar um componente do React

---
+++ Capitulo_08/exemplo.js
@@ -0,0 +1,15 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // Altere o código abaixo desta linha
+    return (
+      <div>
+        <h1>Hello React!</h1>
+      </div>
+    )
+
+    // Altere o código acima desta linha
+  }
+};
