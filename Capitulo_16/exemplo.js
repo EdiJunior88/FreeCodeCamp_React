a5bc4e66ed12357b3593ae96393b54f7bc668b6f
EdiJunior88 <er.moura88@yahoo.com.br>
2022-09-26 20:13:02 -0300

    Usar props padrão

---
+++ Capitulo_16/exemplo.js
@@ -0,0 +1,9 @@
+const ShoppingCart = (props) => {
+  return (
+    <div>
+      <h1>Shopping Cart Component</h1>
+    </div>
+  )
+};
+// Altere o código abaixo desta linha
+ShoppingCart.defaultProps = {items: 0}
