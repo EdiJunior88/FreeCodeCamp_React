96b8be7d8c8d63534194dc47992f79534d3df0d8
EdiJunior88 <er.moura88@yahoo.com.br>
2022-09-26 18:04:43 -0300

    Escrever um componente do React do zero

---
+++ Capitulo_13/exemplo.js
@@ -0,0 +1,15 @@
+// Altere o código abaixo desta linha
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>My First React Component!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<MyComponent />, document.getElementById("challenge-node"));
