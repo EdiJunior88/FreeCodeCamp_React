9c5d0befcdd7e0a7e802c318a9c99871fcab6202
EdiJunior88 <er.moura88@yahoo.com.br>
2022-10-03 17:53:46 -0300

    Usar o método do ciclo de vida componentWillMount

---
+++ Capitulo_32/exemplo.js
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // Altere o código abaixo desta linha
+    console.log("teste")
+    // Altere o código acima desta linha
+  }
+  render() {
+    return <div />
+  }
+};
