07ac92d26fe33247e9b4ffc87e1a877740b0d84d
EdiJunior88 <er.moura88@yahoo.com.br>
2022-10-06 18:01:26 -0300

    Otimizar novas renderizações com shouldComponentUpdate

---
+++ Capitulo_35/exemplo.js
@@ -0,0 +1,44 @@
+class OnlyEvens extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Should I update?');
+    // Altere o código abaixo desta linha
+    if(nextProps.value % 2 === 0) {
+      return true
+    } else {
+      return false
+    }
+    // Altere o código acima desta linha
+  }
+  componentDidUpdate() {
+    console.log('Component re-rendered.');
+  }
+  render() {
+    return <h1>{this.props.value}</h1>;
+  }
+}
+
+class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+    this.addValue = this.addValue.bind(this);
+  }
+  addValue() {
+    this.setState(state => ({
+      value: state.value + 1
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value} />
+      </div>
+    );
+  }
+}
