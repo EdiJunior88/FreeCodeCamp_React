878a94abef371b4fa58d7624cbfa15e4b02910fd
EdiJunior88 <er.moura88@yahoo.com.br>
2022-10-07 21:27:18 -0300

    Dar aos elementos irmãos um atributo chave único

---
+++ Capitulo_45/exemplo.js
@@ -0,0 +1,20 @@
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map((item) => <li key={item}>{item}</li> ); // Altere esta linha
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
