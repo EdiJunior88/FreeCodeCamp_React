9b074e77f0e460e13aa39de8ed1d9735685765c4
EdiJunior88 <er.moura88@yahoo.com.br>
2022-10-07 21:12:39 -0300

    Mudar CSS em linha condicionalmente com base no estado do componente

---
+++ Capitulo_43/exemplo.js
@@ -0,0 +1,34 @@
+class GateKeeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ input: event.target.value })
+  }
+  render() {
+    let inputStyle = {
+      border: '1px solid black'
+    };
+    // Altere o código abaixo desta linha
+    if(this.state.input.length > 15) {
+      inputStyle = {
+        border: '3px solid red'
+      };
+    }
+    // Altere o código acima desta linha
+    return (
+      <div>
+        <h3>Don't Type Too Much:</h3>
+        <input
+          type="text"
+          style={inputStyle}
+          value={this.state.input}
+          onChange={this.handleChange} />
+      </div>
+    );
+  }
+};
