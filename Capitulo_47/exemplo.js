9e0715f24610b9a595d5086a200dc8fafc291822
EdiJunior88 <er.moura88@yahoo.com.br>
2022-10-07 21:39:42 -0300

    Renderizar React no servidor com renderToString

---
+++ Capitulo_47/exemplo.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div/>
+  }
+};
+
+// Altere o código abaixo desta linha
+ReactDOMServer.renderToString(<App />);
